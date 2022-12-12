81c1472c94ef9e4ab1f7b37d78e5329d9cd515d2
Ragul <ragul.ravishankar@gmail.com>
2022-12-12 00:41:50 -0700

    reports services changes

--- services/reports/subscriber.py
+++ services/reports/subscriber.py
@@ -4,6 +4,7 @@ import json
 from minio import Minio
 import random
 import string
+from pathlib import Path
 
 # credentialsPath = '/Users/ragulravisankar/Desktop/plagiarism-checker/plagiarism-check.privatekey.json'
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentialsPath
@@ -14,34 +15,51 @@ subscriber = pubsub_v1.SubscriberClient()
 subscriberPath = 'projects/plagiarism-368919/subscriptions/plagiarism-reports-sub'
 fileName = (''.join(random.choices(string.ascii_letters, k=5)))+'.html'
 
-bucket = os.getenv('BUCKET', "plagiarism-ingestion1107")
+bucket = os.getenv('BUCKET', "plagiarism-ingestion")
 s3Src = os.getenv('S3_SRC', "storage.googleapis.com")
-secretId = os.getenv('SECRET_ID', "GOOG4342SX44CC4OM4OHFJ3B")
-secretKey = os.getenv('SECRET_KEY', "NG8bZ0zVbcaIv4ic+U1TbtNrL3T+zkcNc6DdxdvT")
+secretId = os.getenv('SECRET_ID', "")
+secretKey = os.getenv('SECRET_KEY', "")
 project = os.getenv('PROJECT', "plagiarism-368919")
-subscription = os.getenv('SUBSCRIBER', "plagiarism-tasks-sub")
+subscription = os.getenv('SUBSCRIBER', "plagiarism-reports-sub")
 subscriberPath = f'projects/{project}/subscriptions/{subscription}'
 
 client = Minio(s3Src, access_key=secretId, secret_key=secretKey, secure=False)
 
 def callback(message):
+    uniquePercent = 0
+    count = 0
+    print(f'Received message: {message}')
+    print(f'data: {message.data}')
+    file_name = message.data.decode('utf-8')
+    local_file = os.path.basename(file_name)
+    client.fget_object(bucket, file_name,local_file)
     f = open(fileName,'w')
-    decodedResponse = message.data.decode('utf-8')
-    checkPlagiarismResponse = json.loads(decodedResponse)
-    details = checkPlagiarismResponse["details"]
-    htmlText = "<!DOCTYPE html><html><body>"
-    # htmlText+= '<!-- CSS only --><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel=\"stylesheet" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin=\"anonymous">'
+    local_file = 'temp.txt'
+    # htmlText = "<!DOCTYPE html><html><head><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head><body>"
+    headerText = "<!DOCTYPE html><html>"
+    endText = "</html></body>"
+    htmlText = ""
+    with open(local_file) as fp:
+        for line in fp:
+            checkPlagiarismResponse = json.loads(line)
+            uniquePercent+=checkPlagiarismResponse["uniquePercent"]
+            count+=1
+            details = checkPlagiarismResponse["details"]
+
+            for idx in range(len(details)):
+                if type(details[idx]) is dict:
+                    if details[idx]['unique']=="true":
+                        queryHTML = "<p>"+ details[idx]['query'] + "</p>"
+                        htmlText+=queryHTML
+                    else:
+                        queryHTML = "<p style='color:red'>"+ details[idx]['query'] + "</p>"
+                        htmlText+=queryHTML
+    plagiarismPercentage = f'The plagiarism percentage is {uniquePercent//count}%'
     
-    for idx in range(len(details)):
-        if details[idx]['unique']=="true":
-            queryHTML = "<p>"+ details[idx]['query'] + "</p>"
-            htmlText+=queryHTML
-        else:
-            queryHTML = "<p style='color:red'>"+ details[idx]['query'] + "</p>"
-            htmlText+=queryHTML
+    fp.close()
     
-    htmlText+="</html></body>"
-    f.write(htmlText)
+    result = headerText + f"<h2>{plagiarismPercentage}</h2>" + htmlText + endText
+    f.write(result)
     f.close()
     object_name = fileName
     file_path = fileName
@@ -49,6 +67,7 @@ def callback(message):
     os.remove(fileName)
     message.ack()
 
+callback(None)
 stream_pull_future = subscriber.subscribe(subscriberPath, callback = callback)
 
 with subscriber:                                               
