0bad9e855085f72ce67c0be2e79331c01a374453
nirajmg <niraj@Nirajs-MacBook-Air.local>
2022-12-11 22:01:11 -0700

    file proc changes

--- services/fileproc/file-proc-service.py
+++ services/fileproc/file-proc-service.py
@@ -2,44 +2,48 @@ import os
 from minio import Minio
 from google.cloud import pubsub_v1
 import PyPDF2
-from flask_login import login_required, current_user
+import os
+from google.cloud import pubsub_v1
+import json
+from minio import Minio
+import random
+import string
+import requests 
 
 credentialsPath = 'plagiarism-check.privatekey.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentialsPath
 
 subscriber = pubsub_v1.SubscriberClient()
-subscriberPath = 'projects/plagiarism-368919/subscriptions/plagiarism-tasks-sub-originalfile'
-fileName = (''.join(random.choices(string.ascii_letters, k=5)))+'.html'
 
-bucket = os.getenv('BUCKET', "plagiarism-ingestion1107")
+bucket = os.getenv('BUCKET', "plagiarism-ingestion")
 s3Src = os.getenv('S3_SRC', "storage.googleapis.com")
-secretId = os.getenv('SECRET_ID', "GOOG4342SX44CC4OM4OHFJ3B")
-secretKey = os.getenv('SECRET_KEY', "NG8bZ0zVbcaIv4ic+U1TbtNrL3T+zkcNc6DdxdvT")
+secretId = os.getenv('SECRET_ID', "GOOG7JBEJ7GU76OUS5HTT4RI")
+secretKey = os.getenv('SECRET_KEY', "CV18bXT9flzceG2RVt6TN0AvgS13cuuR6wNfX4Nv")
 project = os.getenv('PROJECT', "plagiarism-368919")
-subscription = os.getenv('SUBSCRIBER', "plagiarism-tasks-sub")
+subscription = os.getenv('SUBSCRIBER_RAW', "plagiarism-raw-sub")
 subscriberPath = f'projects/{project}/subscriptions/{subscription}'
 
 
 
 client = Minio(s3Src, access_key=secretId, secret_key=secretKey)
 
-url = 'https://www.check-plagiarism.com/apis/checkPlag'
-key = '422ccf1360520da2957f319bdd10246f'
+url = os.getenv('PL_URL', "")
+key = os.getenv('API_KEY', "")
 
-x = requests.post(url, json = myobj)
 
 def callback(message):
 	print(f'Received message: {message}')
 	print(f'data: {message.data}')
-	blob_name = message.data.decode('utf-8')
-	storage_client = storage.Client()
-	bucket_access = storage_client.bucket(bucket)
-	blob = bucket_access.blob(blob_name)
+	file_name = message.data.decode('utf-8')
 	
-	pdffile = open(blob, mode='rb')
+	local_file = os.path.basename(file_name)
+	client.fget_object(bucket, file_name,local_file)
+	
+	pdffile = open(local_file, mode='rb')
 	pdfdoc = PyPDF2.PdfFileReader(pdffile)
 
-	file_key = hashlib.md5(open(blob,'rb').read()).hexdigest()
+	json_file = open(f'{local_file}.json', 'a+')
+
 	for i in range(pdfdoc.numPages):
 		current_page = pdfdoc.getPage(i)
 		print("===================")
@@ -51,29 +55,17 @@ def callback(message):
 		form_data['data'] =text_to_be_sent
 
 		r = requests.post(url, data=form_data)
-
-		output_file=dict()
-		output['id'] =file_key
-		output['data']=r.text
-
-		# Serializing json
-		json_object = json.dumps(output_file, indent=4)
- 
-
-		object_name = f'{current_user.email}/files/{secure_filename(file_key)}'
-		result = client.put_object(bucket, object_name, json_object, length=-1, part_size=10*1024*1024,) 
-		flash(f'{file_key} was successfully uploaded', 'success')
-
-
-
-		
-
-
-
+		json_file.write(r.text)
 
 	
+	user=file_name.split('/')[0]
+	object_name = f'{user}/raw/{local_file}'
+	result = client.fput_object(bucket, object_name, json_file) 
+	os.remove(json_file)
+
+
 # subscribe method provides an asynchronous interface for processing its callback
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+streaming_pull_future = subscriber.subscribe(subscriberPath, callback=callback)
 with subscriber:                                           # wrap subscriber in a 'with' block to automatically call close() when done
     try:
         streaming_pull_future.result()                          # going without a timeout will wait & block indefinitely
